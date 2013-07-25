aa05ef9d3c51018595565f6ab1e5fd1564406ac2
McSpider <circuit.spider@gmail.com>
2013-07-24 19:26:09 -0700

    Add a function to display the title for a oc.tc/forum link.

---
+++ _functions/forumlink.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from function_template import *
+import requests
+
+class function(function_template):
+    def __init__(self):
+        function_template.__init__(self)
+        self.type = ["natural"]
+        self.priority = 100
+        self.functionString = "Get the title for oc.tc/forum links."
+        self.hidden = True
+
+
+    def main(self, bot, msgData, funcType):
+        if (funcType == "natural"):
+            message = string.join(msgData["message"])
+            linkMatch = re.findall("(oc\.tc/forums/topics/[A-Za-z0-9-]+[^/])", message, re.IGNORECASE)
+            if not linkMatch: linkMatch = re.findall("(oc\.tc/forums/[A-Za-z0-9-]+[^/])", message, re.IGNORECASE)
+            if linkMatch:
+                for link in linkMatch:
+                    r = requests.get("http://" + link)
+                    if r.status_code != requests.codes.ok:
+                        return False
+
+                    soup = BeautifulSoup(r.text)
+                    if soup.find('title'):
+                        pageTitle = soup.find('title').get_text().strip()
+                        if pageTitle == "Home - Overcast Network Forum":
+                            return False
+                        bot._irc.sendMSG(pageTitle, msgData["target"])
+                        return True
+        
+        return False
+
+
