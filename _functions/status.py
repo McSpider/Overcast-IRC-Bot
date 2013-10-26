d6b4d51d1fc3e6b1d0a3d0be4c53f6b526760fa3
McSpider <circuit.spider@gmail.com>
2013-10-26 10:31:04 -0700

    Add minecraft server status info to status command.

--- _functions/status.py
+++ _functions/status.py
@@ -6,20 +6,40 @@ import requests
 class function(function_template):
     def __init__(self):
         function_template.__init__(self)
-        self.commands = ["status"]
-        self.functionString = "Get the Overcast server status."
+        self.commands = ["status","mcstatus"]
+        self.functionString = "Get the Overcast and minecraft server status."
     
     def main(self, bot, msgData, funcType):
         error = ''
         r = requests.get("https://oc.tc/play")
         if r.status_code != requests.codes.ok:
-            error = 'Request Exception - Code: ' + str(r.status_code)
+            error = 'oc.tc - &05' + str(r.status_code) + "&c"
         else:
             soup = BeautifulSoup(r.text)
             status = soup.find("h3", text=re.compile(".*Players Online.*"))#.contents#[1].strip('\n')
 
             status = soup.find("b", text=["Status"]).findParent('td').findParent('tr').find_all('td')[1].contents[2].strip('\n')
-            bot._irc.sendMSG("%s" % (status), msgData["target"])
+            bot._irc.sendMSG("oc.tc: %s" % (status), msgData["target"])
+            
+        r = requests.get("http://status.mojang.com/check")
+        if r.status_code != requests.codes.ok:
+            error = 'status.mojang.com/check - &05' + str(r.status_code) + "&c"
+        else:
+            result = ""
+            mojang_status = r.json()
+            for x in mojang_status:
+                for key in x:
+                    keyvalue = key
+                    if key != "minecraft.net":
+                        keyvalue = key.split(".",1)[0]
 
-        if error: bot._irc.sendMSG(error, msgData["target"])
+                    if x[key] == "green":
+                        result = result + keyvalue + " &03OK&c, "
+                    else:
+                        result = result + keyvalue + " &050&c, "
+
+            result = colorizer(result.rstrip(', '))
+            bot._irc.sendMSG("%s" % (result), msgData["target"])
+
+        if error: bot._irc.sendMSG(colorizer(error), msgData["target"])
         return True
