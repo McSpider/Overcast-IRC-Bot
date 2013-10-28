6fce390af98e24379f6e88d175596812e944a3af
McSpider <circuit.spider@gmail.com>
2013-10-27 20:04:18 -0700

    Rename formlink function to linktitle.

--- _functions/linktitle.py
+++ _functions/linktitle.py
@@ -7,14 +7,17 @@ class function(function_template):
         function_template.__init__(self)
         self.type = ["natural"]
         self.priority = 100
-        self.functionString = "Get the title for oc.tc/forum links."
+        self.functionString = "Get the title for specific links."
         self.hidden = True
 
 
     def main(self, bot, msgData, funcType):
         if (funcType == "natural"):
             message = string.join(msgData["message"])
-            linkMatch = re.findall("(oc\.tc/forums/topics/[A-Za-z0-9-]+|oc\.tc/forums/[A-Za-z0-9-]+)", message, re.IGNORECASE)
+            linkMatch = re.findall("(oc\.tc/forums/topics/[A-Za-z0-9-]+|oc\.tc/forums/[A-Za-z0-9-]+|redd\.it/[A-Za-z0-9-]+)", message, re.IGNORECASE)
+
+            #reddit\.com/r/[A-Za-z0-9-/_]+
+            #http://www.reddit.com/r/electronics/comments/1pbw93/alarm_clock_i_built_entirely_from_4000_series/
             if linkMatch:
                 for link in linkMatch:
                     r = requests.get("http://" + link)
@@ -24,12 +27,14 @@ class function(function_template):
                     soup = BeautifulSoup(r.text)
                     if soup.find('title'):
                         pageTitle = soup.find('title').get_text().strip()
+
+                        # Ignore specific links/pages
                         if pageTitle == "Home - Overcast Network Forum":
                             continue
                         bot._irc.sendMSG(pageTitle, msgData["target"])
                 
                 return True
-        
+       
         return False
 
 
