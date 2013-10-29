969b45e5bd7e5a21a7a5bfd9888b86d9396188da
McSpider <circuit.spider@gmail.com>
2013-10-29 11:36:14 -0700

    Improve linktitle function.

--- _functions/linktitle.py
+++ _functions/linktitle.py
@@ -14,26 +14,42 @@ class function(function_template):
     def main(self, bot, msgData, funcType):
         if (funcType == "natural"):
             message = string.join(msgData["message"])
-            linkMatch = re.findall("(oc\.tc/forums/topics/[A-Za-z0-9-]+|oc\.tc/forums/[A-Za-z0-9-]+|redd\.it/[A-Za-z0-9-]+)", message, re.IGNORECASE)
 
-            #reddit\.com/r/[A-Za-z0-9-/_]+
-            #http://www.reddit.com/r/electronics/comments/1pbw93/alarm_clock_i_built_entirely_from_4000_series/
-            if linkMatch:
-                for link in linkMatch:
-                    r = requests.get("http://" + link)
-                    if r.status_code != requests.codes.ok:
-                        continue
+            link_matchers = {"Reddit":"(www\.reddit\.com/r/[A-Za-z0-9-/_]+|redd\.it/[A-Za-z0-9-]+)", \
+            "Overcast":"(oc\.tc/forums/topics/[A-Za-z0-9-]+|oc\.tc/forums/[A-Za-z0-9-]+)", \
+            "Imgur":"(imgur\.com/[A-Za-z0-9-/_#]+)"}
 
-                    soup = BeautifulSoup(r.text)
-                    if soup.find('title'):
-                        pageTitle = soup.find('title').get_text().strip()
 
-                        # Ignore specific links/pages
-                        if pageTitle == "Home - Overcast Network Forum":
+            titles = []
+            for key, value in link_matchers.iteritems():
+                linkMatch = re.findall(value, message, re.IGNORECASE)
+
+                if linkMatch:
+                    for link in linkMatch:
+                        r = requests.get("http://" + link)
+                        if r.status_code != requests.codes.ok:
                             continue
-                        bot._irc.sendMSG(pageTitle, msgData["target"])
-                
-                return True
+
+                        soup = BeautifulSoup(r.text)
+                        if soup.find('title'):
+                            pageTitle = soup.find('title').get_text().strip()
+
+                            try:
+                                pageTitle = pageTitle.encode('utf-8')
+                            except UnicodeEncodeError:
+                                print color.red + 'Unicode Error: Unable to encode string! ' + color.clear
+
+                            # Ignore oc.tc 404 pages
+                            if key == "Overcast":
+                                if pageTitle == "Home - Overcast Network Forum":
+                                    continue
+
+                            # Don't print the same title twice
+                            if not pageTitle in titles:
+                                titles.append(pageTitle)
+                                bot._irc.sendMSG(pageTitle, msgData["target"])
+                    
+            return True
        
         return False
 
