4b9a582f443bba83fdf54db4ed44ebbafbdc3696
McSpider <circuit.spider@gmail.com>
2013-11-19 17:10:08 -0800

    Add overcast network issues to the linktitle function, also ignore imgur img links.

--- _functions/linktitle.py
+++ _functions/linktitle.py
@@ -15,25 +15,30 @@ class function(function_template):
         if (funcType == "natural"):
             message = string.join(msgData["message"])
 
-            link_matchers = {"Reddit":"(www\.reddit\.com/r/[A-Za-z0-9-/_]+|redd\.it/[A-Za-z0-9-]+)", \
+            link_matchers = {"Reddit":"(www\.reddit\.com/r/[A-Za-z0-9-_]+/[A-Za-z0-9-/_]+|redd\.it/[A-Za-z0-9-]+)", \
             "Overcast":"(oc\.tc/forums/topics/[A-Za-z0-9-]+|oc\.tc/forums/[A-Za-z0-9-]+)", \
-            "Imgur":"(imgur\.com/[A-Za-z0-9-/_#]+)"}
+            "Imgur":"(imgur\.com/[A-Za-z0-9-/_#]+|i\.imgur\.com/[A-Za-z0-9-/_#\.]+)", \
+            "Oc Issues":"(github\.com/OvercastNetwork/[^\s/]+/issues/[0-9]+)"}
 
 
             titles = []
-            match = False
+            match_found = False
             for key, value in link_matchers.iteritems():
                 linkMatch = re.findall(value, message, re.IGNORECASE)
 
                 if linkMatch:
                     for link in linkMatch:
+                        # Ignore image imgur links since they usually don't have a useful title. (This should be handled differently...)
+                        if re.match("i\.imgur\.com/[A-Za-z0-9-/_#\.]+", link, re.IGNORECASE):
+                            continue
+
                         r = requests.get("http://" + link)
                         if r.status_code != requests.codes.ok:
                             continue
 
                         soup = BeautifulSoup(r.text)
                         if soup.find('title'):
-                            match = True
+                            match_found = True
                             pageTitle = soup.find('title').get_text().strip()
 
                             pageTitle = encode_unicode(pageTitle)
@@ -43,12 +48,17 @@ class function(function_template):
                                 if pageTitle == "Home - Overcast Network Forum":
                                     continue
 
+                            # Ignore imgur 404 pages
+                            if key == "Imgur":
+                                if pageTitle == "imgur: the simple image sharer":
+                                    continue
+
                             # Don't print the same title twice
                             if not pageTitle in titles:
                                 titles.append(pageTitle)
                                 bot._irc.sendMSG(pageTitle, msgData["target"])
                     
-            return match
+            return match_found
        
         return False
 
