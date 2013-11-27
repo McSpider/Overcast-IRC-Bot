5ca1004cef1c40c8a895aee8da6e45c8ebfb91fc
McSpider <circuit.spider@gmail.com>
2013-11-27 10:40:31 -0800

    Add more detailed offline servers info.

--- _functions/status.py
+++ _functions/status.py
@@ -15,15 +15,48 @@ class function(function_template):
         if r.status_code != requests.codes.ok:
             error = 'oc.tc - &05' + str(r.status_code) + "&c"
         else:
+            oc_status = ""
             soup = BeautifulSoup(r.text)
             players_online = soup.find(text=re.compile(".*Players Online.*")).findParent('h3').find_all('small')[0].string.replace('\n','')
-            print repr(players_online)
 
-            status = soup.find("b", text=["Status"]).findParent('td').findParent('tr').find_all('td')[1].contents[2].strip('\n')
+            eu_main_soup = soup.find('div',id="eu-main")
+            us_main_soup = soup.find('div',id="us-main")
+            if us_main_soup:
+                us_status = us_main_soup.find("b", text=["Status"]).findParent('td').findParent('tr').find_all('td')[1].contents[2].strip('\n')
+                oc_status += "US: " + us_status.title()
+            if eu_main_soup:
+                eu_status = eu_main_soup.find("b", text=["Status"]).findParent('td').findParent('tr').find_all('td')[1].contents[2].strip('\n')
+                oc_status += (", " if oc_status else "") + "EU: " + eu_status.title()
+
             if len(players_online) > 0:
-                status = status + " - Online Players: " + players_online
-            bot._irc.sendMSG("oc.tc: %s" % (status), msgData["target"])
+                oc_status = oc_status + " - Online Players: " + players_online
+            bot._irc.sendMSG("%s" % (oc_status), msgData["target"])
+
+            ## Offline servers
+            eu_servers = []
+            us_servers = []
+            us_soup = soup.find('div',id="us")
+            eu_soup = soup.find('div',id="eu")
+
+            us_offline = us_soup.find_all(text='(Offline)')
+            for item in us_offline:
+                offline_server = item.parent.parent.contents[0].replace('\n','')
+                if offline_server:
+                    us_servers.append(offline_server)
+
+            eu_offline = eu_soup.find_all(text='(Offline)')
+            for item in eu_offline:
+                offline_server = item.parent.parent.contents[0].replace('\n','')
+                if offline_server:
+                    eu_servers.append(offline_server)
+
+            if len(us_servers) > 0:
+                bot._irc.sendMSG("US Servers Offline: " + prettyListString(us_servers, " & ", cc = color.irc_red), msgData["target"])
+            if len(eu_servers) > 0:
+                bot._irc.sendMSG("EU Servers Offline: " + prettyListString(eu_servers, " & ", cc = color.irc_red), msgData["target"])
+            ##
             
+
         r = requests.get("http://status.mojang.com/check")
         if r.status_code != requests.codes.ok:
             error = 'status.mojang.com/check - &05' + str(r.status_code) + "&c"
