d013935b8507836cfccec319c161f2bd5d3205e4
McSpider <circuit.spider@gmail.com>
2013-11-30 10:21:09 -0800

    Add ask function.

---
+++ _functions/ask.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from function_template import *
+
+
+class function(function_template):
+    def __init__(self):
+        function_template.__init__(self)
+        self.commands = ["ask"]
+        self.function_string = "Prompt a person to ask the freaking question already."
+
+        # From +Brottweiler in #overcastnetwork
+        self.ask_message = "If you have a question, please just ask it. Don't look for staff or topic experts. Don't ask to ask or ask if people are awake or available. Just ask the question to the channel straight out, and wait patiently for a reply."
+
+    def main(self, bot, msg_data, func_type):
+        if len(msg_data["message"]) > 1:
+            argument = msg_data["message"][1]
+            bot._irc.sendMSG("%s: %s" % (argument, self.ask_message), msg_data["target"])
+        else:
+            bot._irc.sendMSG(self.ask_message, msg_data["target"])
+        return True
