2580c94934167a98214fadb33c840e5bf02fdc7e
McSpider <circuit.spider@gmail.com>
2014-03-28 09:36:22 -0700

    Make ping function slightly smarter.

--- _functions/ping.py
+++ _functions/ping.py
@@ -6,8 +6,11 @@ class function(function_template):
     def __init__(self):
         function_template.__init__(self)
         self.commands = ["ping"]
-        self.function_string = "Ping the bot or other users."
+        self.function_string = "Ask the bot for a ping reply."
 
     def main(self, bot, msg_data, func_type):
-        bot._irc.sendMSG("pong", msg_data["target"])
+        if msg_data["sender"] == msg_data["target"]:
+            bot._irc.sendMSG("pong", msg_data["target"])
+        else:
+            bot._irc.sendMSG("%s: pong" % (msg_data["sender"]), msg_data["target"])
         return True
