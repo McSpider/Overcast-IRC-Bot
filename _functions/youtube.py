d83b9e3e02bf33896a66beff9f8333eff22969de
McSpider <circuit.spider@gmail.com>
2015-06-02 11:31:41 -0700

    Youtube has depreciated v2 of their API, update to use v3. (Fixes #9)

--- _functions/youtube.py
+++ _functions/youtube.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+## NOTE: For this function to work you will need a valid API key.
+
 from function_template import *
 import requests
+import json
 
 class function(function_template):
     def __init__(self):
         function_template.__init__(self)
         self.type = ["natural","command"]
-        self.commands = ["youtube","yt"] 
+        self.commands = ["youtube","yt"]
         self.priority = 100
         self.function_string = "Get youtube movies and movie info."
 
@@ -20,7 +23,7 @@ class function(function_template):
             if not youtube_match: youtube_match = re.findall("youtu\.be/([A-Za-z0-9-_]+)", message, re.IGNORECASE)
             if youtube_match:
                 for video_id in youtube_match:
-                    print "Getting YT Video: " + str(video_id)
+                    log.debug("Getting YT Video: " + str(video_id))
                     video_info = self.getVideoInfo(bot, video_id)
                     if video_info and not video_info == 'RequestException':
                         bot.irc.sendMSG(video_info, msg_data["target"])
@@ -34,7 +37,7 @@ class function(function_template):
                 if not youtube_match: youtube_match = re.findall("youtu\.be/([A-Za-z0-9-_]+)", message, re.IGNORECASE)
                 if youtube_match:
                     for video_id in youtube_match:
-                        print "Getting YT Video: " + str(video_id)
+                        log.debug("Getting YT Video: " + str(video_id))
                         video_info = self.getVideoInfo(bot, video_id)
                         if video_info == 'APIException':
                             bot.irc.sendMSG('Error: Failed to query youtube API', msg_data["target"])
@@ -48,25 +51,27 @@ class function(function_template):
             return False
 
     def getVideoInfo(self, bot, video_id):
-        r = requests.get("https://gdata.youtube.com/feeds/api/videos/%s?v=2" % video_id, headers = bot.http_header)
+        r = requests.get("https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=%s&fields=items(snippet,statistics)&key=%s" % (video_id, "YOUR API KEY HERE"), headers = bot.http_header)
+
+        # log.debug(r.status_code)
         if r.status_code != requests.codes.ok:
             return 'APIException'
-        youtube_soup = BeautifulSoup(r.text)
-        if youtube_soup.find(text='InvalidRequestUriException'):
-            return 'RequestException'
+        # log.debug(r.text)
+        youtube_soup = json.loads(r.text)
 
-        if youtube_soup.find('title'):
-            youtube_title = youtube_soup.find('title').get_text()
+
+        # if youtube_soup.find(text='InvalidRequestUriException'):
+        #     return 'RequestException'
+
+        if youtube_soup['items'][0]['snippet']:
+            youtube_title = youtube_soup['items'][0]['snippet']['title']
+            youtube_author = ' by: ' + youtube_soup['items'][0]['snippet']['channelTitle']
             youtube_rating = ""
-            youtube_author = ""
 
-            if youtube_soup.find('author').find('name'):
-                youtube_author = ' by: ' + youtube_soup.find('author').find('name').get_text()
-            if youtube_soup.find('yt:rating'):
-                youtube_rating = colorizer(" - Likes:&03 %s&c Dislikes:&05 %s" % (youtube_soup.find('yt:rating')['numlikes'], youtube_soup.find('yt:rating')['numdislikes']))
+            if youtube_soup['items'][0]['statistics']:
+                youtube_rating = colorizer(" - Likes:&03 %s&c Dislikes:&05 %s" % (youtube_soup['items'][0]['statistics']['likeCount'],youtube_soup['items'][0]['statistics']['dislikeCount']))
+
             return ("%s%s%s" % (youtube_title, youtube_author, youtube_rating)).encode('utf-8')
 
         return False
-
-
 
